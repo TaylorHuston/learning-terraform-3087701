cd4da7542df56ed69176483fa5422f3a9a097f66
Taylor Huston <taylor.a.huston@gmail.com>
2023-10-14 13:29:42 -0400

    Change AZ

--- main.tf
+++ main.tf
@@ -20,17 +20,17 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
